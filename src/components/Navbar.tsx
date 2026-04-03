5c3f8371cfc2953b76c05f0bf6c69eefc622f1a2
Joao Spolavori <joao@workingcolours.com.au>
2026-04-03 23:13:31 +1100

    Increase navbar height and logo size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,11 +36,11 @@ export default function Navbar() {
   return (
     <header className="bg-white shadow-sm sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-20">
 
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2">
-            <Image src="/logo.png" alt="Working Colours Painting Services" width={120} height={40} className="h-10 w-auto" />
+            <Image src="/logo.png" alt="Working Colours Painting Services" width={180} height={60} className="h-14 w-auto" />
           </Link>
 
           {/* Desktop Nav */}
